fb40e23f3ea0f035d0508d9d5c28b3fbaf161f63
Mthokozisi <191776689+zeff2421@users.noreply.github.com>
2025-05-02 19:46:03 +0200

    Add the necessary constant values

--- main_test.cpp
+++ main_test.cpp
@@ -5,3 +5,10 @@
 #include <queue>
 #include <map>
 #include <iomanip>
+
+using namespace std;
+
+const char SHORT_ROUTE = 'S';
+const char LONG_ROUTE = 'L';
+const char CITY_ROUTE = 'C';
+const int PASSENGERS_PER_TAXI = 5;
